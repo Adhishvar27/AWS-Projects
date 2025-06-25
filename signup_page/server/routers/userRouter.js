8e9794fdb3b5718f9ffb006072065b8165119c47
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-25 22:35:43 +0530

    Add files via upload

--- signup_page/server/routers/userRouter.js
+++ signup_page/server/routers/userRouter.js
@@ -4,5 +4,6 @@ const router=express.Router();
 const usercontroller=require('../controllers/usercontroller');
 
 router.post('/signup',usercontroller.addtousers);
+router.post('/login',usercontroller.checklogin);
 
 module.exports=router;
