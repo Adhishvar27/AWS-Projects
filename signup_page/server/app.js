d6f8b8bb3457a1a429da49191bbd7628040632bf
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-25 20:12:27 +0530

    Create app.js

---
+++ signup_page/server/app.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const path=require('path');
+const cors=require('cors');
+const app=express();
+
+app.use(express.json());
+
+app.use(cors());
+app.use(express.static(path.join(__dirname,'..','public')));
+
+const userrouter=require('./routers/userRouter');
+app.use('/users',userrouter);
+
+app.use('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','public','frontpage.html'));
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running');
+})
