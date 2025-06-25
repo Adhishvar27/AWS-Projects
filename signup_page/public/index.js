64b0c5915e44f49c19265c6402ca0761d56cc7df
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-25 20:11:28 +0530

    Create index.js

---
+++ signup_page/public/index.js
@@ -0,0 +1,26 @@
+
+document.getElementById('signUpForm').addEventListener('submit',onSubmitFunction);
+async function onSubmitFunction(event){
+        event.preventDefault();
+
+        const form = event.target;
+        const myObj={
+        name:form.name.value,
+        email:form.email.value,
+        password:form.password.value
+    };
+    try {
+    const response= await fetch('http://localhost:3000/users/signup',{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(myObj)
+    });
+    if(!response.ok){
+        console.log('error');
+    }
+    } catch (error) {
+        console.error(error);
+    }
+}
