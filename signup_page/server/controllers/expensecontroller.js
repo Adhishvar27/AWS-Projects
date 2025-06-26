d3853de0182300b19a1dc85f32be06e80b87af06
Adhishvar27 <adhishvar.46@gmail.com>
2025-06-26 19:29:28 +0530

    Add files via upload

---
+++ signup_page/server/controllers/expensecontroller.js
@@ -0,0 +1,42 @@
+const ExpenseTable=require('../modules/expenseTable');
+
+const addtoexpense=async(req,res)=>{
+    try {
+        const {amount,category,description}=req.body;
+        const insertintoexpense=await ExpenseTable.create({
+            amount,
+            category,
+            description
+        });
+        res.status(200).json({insertintoexpense,message:'Expense added to the table'});
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
+
+const retriveexpense=async (req,res)=>{
+    try {
+        const listOfExpenses=await ExpenseTable.findAll();
+        res.status(200).json(listOfExpenses);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
+
+const deleteexpense=async(req,res)=>{
+    try {
+        const id=parseInt(req.params.id);
+        const deleteValue=await ExpenseTable.destroy({
+            where:{id}
+        })
+        res.status(200).json({deleteValue,message:'Expense deleted from the Table'});
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
+
+module.exports={
+    addtoexpense,
+    retriveexpense,
+    deleteexpense
+}
